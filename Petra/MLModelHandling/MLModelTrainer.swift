9193bfc7ec11e21a2d075cb211e95c5f5e1f6ca5
J Rogel-Salazar <jrogel.datascience@icloud.com>
2022-08-09 16:14:33 +0100

    Training the Model

--- Petra/MLModelHandling/MLModelTrainer.swift
+++ Petra/MLModelHandling/MLModelTrainer.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2022 Razeware LLC
-/// 
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-/// 
+///
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-/// 
+///
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -38,45 +38,23 @@ enum MLModelTrainer {
   private static var subscriptions = Set<AnyCancellable>()
   static func trainModel(using styleImage: URL, validationImage: URL, sessionDir: URL, onCompletion: @escaping (URL?) -> Void) {
     // 1
-    let dataSource = MLStyleTransfer.DataSource.images(
+    let datasource = MLStyleTransfer.DataSource.images(
       styleImage: styleImage,
       contentDirectory: Constants.Path.trainingImagesDir ?? Bundle.main.bundleURL,
       processingOption: nil)
     // 2
-    let sessionParams = MLTrainingSessionParameters(
+    let sessioParams = MLTrainingSessionParameters(
       sessionDirectory: sessionDir,
-      reportInterval: Constants.MLSession.reportInterval,
-      checkpointInterval: Constants.MLSession.checkpointInterval,
+      reportInterval: Constants.MLSession.checkpointInterval,
       iterations: Constants.MLSession.iterations)
     // 3
-    let modelParams = MLStyleTransfer.ModelParameters(
+    let modelParams = MLStyleTrasfer.ModelParameters(
       algorithm: .cnn,
-      validation: .content(validationImage),
+      validation: .content(validation),
       maxIterations: Constants.MLModelParam.maxIterations,
       textelDensity: Constants.MLModelParam.styleDensity,
       styleStrength: Constants.MLModelParam.styleStrength)
-    // 4
-    guard let job = try? MLStyleTransfer.train(
-      trainingData: dataSource,
-      parameters: modelParams,
-      sessionParameters: sessionParams) else {
-      onCompletion(nil)
-      return
-    }
-    // 5
-    let modelPath = sessionDir.appendingPathComponent(Constants.Path.modelFileName)
-    job.result.sink(receiveCompletion: { result in
-      debugPrint(result)
-    }, receiveValue: { model in
-      do {
-        try model.write(to: modelPath)
-        onCompletion(modelPath)
-        return
-      } catch {
-        debugPrint("Error saving ML Model: \(error.localizedDescription)")
-      }
-      onCompletion(nil)
-    })
-    .store(in: &subscriptions)
+    
+    return onCompletion(nil)
   }
 }
--- Petra/MLModelHandling/MLPredictor.swift
+++ Petra/MLModelHandling/MLPredictor.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2022 Razeware LLC
-/// 
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-/// 
+///
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-/// 
+///
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -37,34 +37,6 @@ import CoreML
 
 enum MLPredictor {
   static func predictUsingModel(_ modelPath: URL, inputImage: UIImage, onCompletion: @escaping (UIImage?) -> Void) {
-    // 1
-    guard
-      let compiledModel = try? MLModel.compileModel(at: modelPath),
-      let mlModel = try? MLModel.init(contentsOf: compiledModel)
-    else {
-      debugPrint("Error reading the ML Model")
-      return onCompletion(nil)
-    }
-    // 2
-    let imageOptions: [MLFeatureValue.ImageOption: Any] = [
-      .cropAndScale: VNImageCropAndScaleOption.centerCrop.rawValue
-    ]
-    guard
-      let cgImage = inputImage.cgImage,
-      let imageConstraint = mlModel.modelDescription.inputDescriptionsByName["image"]?.imageConstraint,
-      let inputImg = try? MLFeatureValue(cgImage: cgImage, constraint: imageConstraint, options: imageOptions),
-      let inputImage = try? MLDictionaryFeatureProvider(dictionary: ["image": inputImg])
-    else {
-      return onCompletion(nil)
-    }
-    // 3
-    guard
-      let stylizedImage = try? mlModel.prediction(from: inputImage),
-      let imgBuffer = stylizedImage.featureValue(for: "stylizedImage")?.imageBufferValue
-    else {
-      return onCompletion(nil)
-    }
-    let stylizedUIImage = UIImage(withCVImageBuffer: imgBuffer)
-    return onCompletion(stylizedUIImage)
+    return onCompletion(nil)
   }
 }
